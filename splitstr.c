dcf81528beef0e95bb6c60543cdf61e7ff58e84d
Katalyst99 <katleho333@gmail.com>
2023-07-26 08:26:30 -0700

    Demo

---
+++ splitstr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * split_tok - splits string into an array of tokens
+ * @line: The line of text.
+ * @nbytes: The number of bytes
+ * Return: Array of pointers to the tokens in line.
+ */
+char **split_tok(char *line, int nbytes)
+{
+	char *lcpy;
+	char *token, **tok_arr;
+	int tks = 0, i = 0;
+	char *dlimit = " \n";
+	
+	if (line == NULL)
+	{
+		return (NULL);
+	}
+	lcpy = malloc(sizeof(char) * nbytes);
+	_strcpy(lcpy, line);
+
+	token = strtok(line, dlimit);
+	while(token)
+	{
+		token = strtok(NULL, dlimit);
+		tks++;
+	}
+	tks++;
+
+	tok_arr = malloc(sizeof(char*) * tks);
+	if (tok_arr == NULL)
+	{
+		return (NULL);
+	}
+
+	token = strtok(lcpy, dlimit);
+	for (i = 0; token; i++)
+	{
+		tok_arr[i] = malloc(sizeof(char) * (_strlen(token)));
+		if (tok_arr[i] == NULL)
+		{
+			return (NULL);
+		}
+		_strcpy(tok_arr[i], token);
+		token = strtok(NULL, dlimit);
+	}
+	tok_arr[i] = NULL;
+	return(tok_arr);
+}
