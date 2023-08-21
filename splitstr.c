1746cdbbf50af48b9314c9df9fcfc7f50d3fccfe
Katalyst99 <katleho333@gmail.com>
2023-08-21 03:59:30 -0700

    Fix

--- splitstr.c
+++ splitstr.c
@@ -8,16 +8,15 @@
 char **split_tok(char *line, int nbytes)
 {
 	char *lcpy;
-	char *token, **tok_arr;
-	int tks = 0, i = 0;
+	char *token = NULL, **tok_arr = NULL;
+	int tks = 0, i, j;
 	char *dlimit = " \n";
 
 	if (line == NULL)
 	{
 		return (NULL);
 	}
-	lcpy = malloc(sizeof(char) * nbytes);
-	_strcpy(lcpy, line);
+	lcpy = _strdup(line);
 
 	token = strtok(line, dlimit);
 	while (token)
@@ -27,9 +26,10 @@ char **split_tok(char *line, int nbytes)
 	}
 	tks++;
 
-	tok_arr = malloc(sizeof(char) * tks);
+	tok_arr = malloc(sizeof(char *) * tks);
 	if (tok_arr == NULL)
 	{
+		free(lcpy);	
 		return (NULL);
 	}
 
@@ -39,11 +39,18 @@ char **split_tok(char *line, int nbytes)
 		tok_arr[i] = malloc(sizeof(char) * (_strlen(token)));
 		if (tok_arr[i] == NULL)
 		{
+			for (j = 0; j < i; j++)
+			{
+				free(tok_arr[j]);
+			}
+			free(lcpy);
+			free(tok_arr);
 			return (NULL);
 		}
-		_strcpy(tok_arr[i], token);
+		tok_arr[i] = _strdup(token);
 		token = strtok(NULL, dlimit);
 	}
 	tok_arr[i] = NULL;
+	free(lcpy);
 	return (tok_arr);
 }
